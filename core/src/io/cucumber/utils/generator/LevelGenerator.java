e8a438d707b7e2764414c9e1f475576e9e97ac72
Sisipun <sisipun@gmail.com>
2021-01-08 19:30:21 +0300

    time event + size + balance generation

--- core/src/io/cucumber/utils/generator/LevelGenerator.java
+++ core/src/io/cucumber/utils/generator/LevelGenerator.java
@@ -2,11 +2,10 @@ package io.cucumber.utils.generator;
 
 import com.badlogic.gdx.utils.Array;
 
-import java.util.Arrays;
-import java.util.Random;
-
+import io.cucumber.utils.random.Random;
 import io.cucumber.utils.event.GenerateEnemyTimeEvent;
 import io.cucumber.utils.event.IncreaseBalanceTimeEvent;
+import io.cucumber.utils.event.TimeEvent;
 import io.cucumber.utils.storage.GameStorage;
 import io.cucumber.utils.storage.defender.DefenderType;
 import io.cucumber.utils.storage.enemy.EnemyType;
@@ -14,188 +13,55 @@ import io.cucumber.utils.storage.enemy.EnemyType;
 public class LevelGenerator {
 
     private final Random random;
+    private final LevelMapGenerator levelMapGenerator;
 
     public LevelGenerator() {
         this.random = new Random();
+        this.levelMapGenerator = new LevelMapGenerator(random);
     }
 
     public Level generate(int width, GameStorage storage) {
-        return new Level(
-                100,
-                45,
-                400,
-                Array.with(storage.getDefender(DefenderType.BASE), storage.getDefender(DefenderType.SMALL)),
-                Array.with(
-                        new GenerateEnemyTimeEvent(1, storage.getEnemy(EnemyType.BASE)),
-                        new IncreaseBalanceTimeEvent(5, 50),
-                        new GenerateEnemyTimeEvent(10, storage.getEnemy(EnemyType.SMALL)),
-                        new IncreaseBalanceTimeEvent(15, 50),
-                        new GenerateEnemyTimeEvent(25, storage.getEnemy(EnemyType.BASE)),
-                        new IncreaseBalanceTimeEvent(30, 50),
-                        new IncreaseBalanceTimeEvent(35, 50),
-                        new GenerateEnemyTimeEvent(40, storage.getEnemy(EnemyType.SMALL))
-                ),
-                generateLevelMap(width, 30, 2)
-        );
-    }
+        int timeInSeconds = random.nextInt(1, 5) * 50;
+        int height = timeInSeconds / 2 + random.nextInt(-10, 10);
+        int border = random.nextInt(1, 3);
+        int balance = height * (5 + random.nextInt(-1, 1));
+        Array<TimeEvent> timeEvents = new Array<>();
 
-    public LevelMap generateLevelMap(int width, int height, int border) {
-        if (width <= 0 || height <= 0) {
-            return new LevelMap(new LevelBlockType[0][0], 0, 0);
-        }
-
-        LevelBlockType[][] map = new LevelBlockType[width][height];
-        Array<Fork> forks = new Array<>();
-        int startPositionX = random.nextInt(width);
-        int startPositionY = height - border - 1;
-        int currentPositionX = startPositionX;
-        int currentPositionY = startPositionY;
-
-        // Initialize area
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                int type = random.nextInt(1000);
-                if (type == 0) {
-                    map[i][j] = LevelBlockType.WATER;
-                } else if (type == 1) {
-                    map[i][j] = LevelBlockType.BUILDING;
-                } else {
-                    map[i][j] = LevelBlockType.LAND;
-                }
+        int startEventGapInSeconds = 10;
+        int eventsGapInSeconds = 5;
+        int lastEvenTime = 0;
+        for (int i = startEventGapInSeconds; i < timeInSeconds - eventsGapInSeconds; i++) {
+            if (lastEvenTime + eventsGapInSeconds > i) {
+                continue;
             }
-        }
 
-        // Create main road
-        while (currentPositionY - border - 1 > 0) {
-            LevelBlockType direction = random.nextInt(2) == 0 ? LevelBlockType.ROAD_LEFT : LevelBlockType.ROAD_RIGHT;
-            int size = random.nextInt(LevelBlockType.ROAD_LEFT.equals(direction) ? currentPositionX : width - currentPositionX);
-            if (random.nextInt(height / 4) == 0 && size > 0) {
-                forks.add(new Fork(
-                        currentPositionX,
-                        currentPositionY,
-                        direction.equals(LevelBlockType.ROAD_LEFT) ? LevelBlockType.ROAD_RIGHT : LevelBlockType.ROAD_LEFT)
+            int eventType = random.nextInt(3);
+            if (eventType == 0) {
+                timeEvents.add(new GenerateEnemyTimeEvent(
+                        i,
+                        storage.getEnemy(EnemyType.values()[random.nextInt(EnemyType.values().length)]))
+                );
+            } else if (eventType == 1) {
+                timeEvents.add(new IncreaseBalanceTimeEvent(
+                        i,
+                        (random.nextInt(1) + 2) * 50)
                 );
             }
-            for (int i = 0; i < size; i++) {
-                map[currentPositionX][currentPositionY] = direction;
-                if (LevelBlockType.ROAD_LEFT.equals(direction)) {
-                    currentPositionX--;
-                } else {
-                    currentPositionX++;
-                }
-            }
-
-            map[currentPositionX][currentPositionY] = LevelBlockType.ROAD_DOWN;
-            currentPositionY--;
-            map[currentPositionX][currentPositionY] = LevelBlockType.ROAD_DOWN;
-            currentPositionY--;
+            lastEvenTime = i;
         }
 
-        while (currentPositionY - border > 0) {
-            map[currentPositionX][currentPositionY] = LevelBlockType.ROAD_DOWN;
-            currentPositionY--;
-        }
+        timeEvents.add(new GenerateEnemyTimeEvent(
+                lastEvenTime + eventsGapInSeconds,
+                storage.getEnemy(EnemyType.values()[random.nextInt(EnemyType.values().length)]))
+        );
 
-        map[currentPositionX][currentPositionY] = LevelBlockType.ROAD_END;
-
-        // Create forks
-        for (int i = 0; i < forks.size; i++) {
-            Fork fork = forks.get(i);
-            createHorizontalFork(fork.x, fork.y, currentPositionX, currentPositionY, fork.direction,
-                    map, width);
-        }
-
-        return new LevelMap(map, startPositionX, startPositionY);
+        return new Level(
+                100,
+                timeInSeconds,
+                balance,
+                Array.with(storage.getDefender(DefenderType.BASE), storage.getDefender(DefenderType.SMALL)),
+                timeEvents,
+                levelMapGenerator.generate(width, height, border)
+        );
     }
-
-    private void createHorizontalFork(int currentX, int currentY, int endX, int endY,
-                                      LevelBlockType direction, LevelBlockType[][] map, int width) {
-        // Check horizontal fork conditions
-        if (!Arrays.asList(LevelBlockType.ROAD_LEFT, LevelBlockType.ROAD_RIGHT).contains(direction)) {
-            return;
-        }
-        if ((LevelBlockType.ROAD_LEFT.equals(direction) && currentX <= 0) ||
-                (LevelBlockType.ROAD_RIGHT.equals(direction) && currentX >= width)) {
-            return;
-        }
-        if (currentY - 1 <= endY) {
-            return;
-        }
-
-        // Initialize fork
-        map[currentX][currentY] = LevelBlockType.ROAD_HORIZONTAL_RAND;
-        if (LevelBlockType.ROAD_LEFT.equals(direction)) {
-            currentX--;
-        } else if (LevelBlockType.ROAD_RIGHT.equals(direction)) {
-            currentX++;
-        }
-
-        while (currentY - 1 > endY) {
-            int size = random.nextInt(LevelBlockType.ROAD_LEFT.equals(direction) ? currentX : width - currentX);
-            for (int i = 0; i < size; i++) {
-                if (isRoad(map[currentX][currentY])) {
-                    return;
-                }
-                map[currentX][currentY] = direction;
-                if (LevelBlockType.ROAD_LEFT.equals(direction)) {
-                    currentX--;
-                } else {
-                    currentX++;
-                }
-            }
-            direction = random.nextInt(2) == 0 ? LevelBlockType.ROAD_LEFT : LevelBlockType.ROAD_RIGHT;
-
-            map[currentX][currentY] = LevelBlockType.ROAD_DOWN;
-            currentY--;
-            if (isRoad(map[currentX][currentY])) {
-                return;
-            }
-            map[currentX][currentY] = LevelBlockType.ROAD_DOWN;
-            currentY--;
-            if (isRoad(map[currentX][currentY])) {
-                return;
-            }
-        }
-
-        while (currentY > endY) {
-            map[currentX][currentY] = LevelBlockType.ROAD_DOWN;
-            currentY--;
-            if (isRoad(map[currentX][currentY])) {
-                return;
-            }
-        }
-
-        while (currentX > endX) {
-            map[currentX][currentY] = LevelBlockType.ROAD_LEFT;
-            currentX--;
-        }
-
-        while (currentX < endX) {
-            map[currentX][currentY] = LevelBlockType.ROAD_RIGHT;
-            currentX++;
-        }
-    }
-
-    private boolean isRoad(LevelBlockType levelBlockType) {
-        return Arrays.asList(
-                LevelBlockType.ROAD_UP,
-                LevelBlockType.ROAD_DOWN,
-                LevelBlockType.ROAD_LEFT,
-                LevelBlockType.ROAD_RIGHT,
-                LevelBlockType.ROAD_HORIZONTAL_RAND,
-                LevelBlockType.ROAD_END).contains(levelBlockType);
-    }
-
-    static class Fork {
-        private int x;
-        private int y;
-        private LevelBlockType direction;
-
-        public Fork(int x, int y, LevelBlockType direction) {
-            this.x = x;
-            this.y = y;
-            this.direction = direction;
-        }
-    }
-
 }
---
+++ core/src/io/cucumber/utils/random/Random.java
@@ -0,0 +1,18 @@
+package io.cucumber.utils.random;
+
+public class Random {
+
+    private final java.util.Random random;
+
+    public Random() {
+        this.random = new java.util.Random();
+    }
+
+    public int nextInt(int from, int to) {
+        return to - from == 0 ? 0 : this.random.nextInt(to - from) + from;
+    }
+
+    public int nextInt(int to) {
+        return to == 0 ? 0 : this.random.nextInt(to);
+    }
+}
